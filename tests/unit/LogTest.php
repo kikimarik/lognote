cba232dae819f74dde791abd00b307788274554e
kikimarik <kikimarik@inbox.ru>
2022-06-19 18:07:39 +0300

    Unit tests - add testReceiveFatal - add testReceiveWarning - add testReceiveNotice - add testReceiveInfo - add testReceiveDebug - move codeception dependencies to require-dev

--- tests/unit/LogTest.php
+++ tests/unit/LogTest.php
@@ -56,6 +56,44 @@ final class LogTest extends Unit
     }
 
     /**
+     * @dataProvider testReceiveFatalDataProvider
+     * @param string $foo
+     * @param string $bar
+     * @param LogLevel $allowedLevel
+     * @param array $expected
+     * @return void
+     */
+    public function testReceiveFatal(string $foo, string $bar, LogLevel $allowedLevel, array $expected): void
+    {
+        $target = new FakeLogTarget();
+        $log = new Log(
+            $target,
+            new FakeLogLineFormat(self::SEPARATOR),
+            $allowedLevel
+        );
+        $log->receiveFatal(new FakeLogLine($foo, $bar));
+        $this->assertEquals($expected, $target->pullData());
+    }
+
+    public function testReceiveFatalDataProvider(): array
+    {
+        return [
+            /* When allowed debug */
+            ["foo", "bar", new DebugLogLevel(), ["fatal" . self::SEPARATOR . "foo" . self::SEPARATOR . "bar"]],
+            /* When allowed info */
+            ["foo", "bar", new InfoLogLevel(), ["fatal" . self::SEPARATOR . "foo" . self::SEPARATOR . "bar"]],
+            /* When allowed notice */
+            ["foo", "bar", new NoticeLogLevel(), ["fatal" . self::SEPARATOR . "foo" . self::SEPARATOR . "bar"]],
+            /* When allowed warning */
+            ["foo", "bar", new WarningLogLevel(), ["fatal" . self::SEPARATOR . "foo" . self::SEPARATOR . "bar"]],
+            /* When allowed error */
+            ["foo", "bar", new ErrorLogLevel(), ["fatal" . self::SEPARATOR . "foo" . self::SEPARATOR . "bar"]],
+            /* When allowed fatal */
+            ["foo", "bar", new FatalLogLevel(), ["fatal" . self::SEPARATOR . "foo" . self::SEPARATOR . "bar"]],
+        ];
+    }
+
+    /**
      * @dataProvider testReceiveErrorDataProvider
      * @param string $foo
      * @param string $bar
@@ -88,6 +126,158 @@ final class LogTest extends Unit
             ["foo", "bar", new WarningLogLevel(), ["error" . self::SEPARATOR . "foo" . self::SEPARATOR . "bar"]],
             /* When allowed error */
             ["foo", "bar", new ErrorLogLevel(), ["error" . self::SEPARATOR . "foo" . self::SEPARATOR . "bar"]],
+            /* When allowed fatal */
+            ["foo", "bar", new FatalLogLevel(), []],
+        ];
+    }
+
+    /**
+     * @dataProvider testReceiveWarningDataProvider
+     * @param string $foo
+     * @param string $bar
+     * @param LogLevel $allowedLevel
+     * @param array $expected
+     * @return void
+     */
+    public function testReceiveWarning(string $foo, string $bar, LogLevel $allowedLevel, array $expected): void
+    {
+        $target = new FakeLogTarget();
+        $log = new Log(
+            $target,
+            new FakeLogLineFormat(self::SEPARATOR),
+            $allowedLevel
+        );
+        $log->receiveWarning(new FakeLogLine($foo, $bar));
+        $this->assertEquals($expected, $target->pullData());
+    }
+
+    public function testReceiveWarningDataProvider(): array
+    {
+        return [
+            /* When allowed debug */
+            ["foo", "bar", new DebugLogLevel(), ["warning" . self::SEPARATOR . "foo" . self::SEPARATOR . "bar"]],
+            /* When allowed info */
+            ["foo", "bar", new InfoLogLevel(), ["warning" . self::SEPARATOR . "foo" . self::SEPARATOR . "bar"]],
+            /* When allowed notice */
+            ["foo", "bar", new NoticeLogLevel(), ["warning" . self::SEPARATOR . "foo" . self::SEPARATOR . "bar"]],
+            /* When allowed warning */
+            ["foo", "bar", new WarningLogLevel(), ["warning" . self::SEPARATOR . "foo" . self::SEPARATOR . "bar"]],
+            /* When allowed error */
+            ["foo", "bar", new ErrorLogLevel(), []],
+            /* When allowed fatal */
+            ["foo", "bar", new FatalLogLevel(), []],
+        ];
+    }
+
+    /**
+     * @dataProvider testReceiveNoticeDataProvider
+     * @param string $foo
+     * @param string $bar
+     * @param LogLevel $allowedLevel
+     * @param array $expected
+     * @return void
+     */
+    public function testReceiveNotice(string $foo, string $bar, LogLevel $allowedLevel, array $expected): void
+    {
+        $target = new FakeLogTarget();
+        $log = new Log(
+            $target,
+            new FakeLogLineFormat(self::SEPARATOR),
+            $allowedLevel
+        );
+        $log->receiveNotice(new FakeLogLine($foo, $bar));
+        $this->assertEquals($expected, $target->pullData());
+    }
+
+    public function testReceiveNoticeDataProvider(): array
+    {
+        return [
+            /* When allowed debug */
+            ["foo", "bar", new DebugLogLevel(), ["notice" . self::SEPARATOR . "foo" . self::SEPARATOR . "bar"]],
+            /* When allowed info */
+            ["foo", "bar", new InfoLogLevel(), ["notice" . self::SEPARATOR . "foo" . self::SEPARATOR . "bar"]],
+            /* When allowed notice */
+            ["foo", "bar", new NoticeLogLevel(), ["notice" . self::SEPARATOR . "foo" . self::SEPARATOR . "bar"]],
+            /* When allowed warning */
+            ["foo", "bar", new WarningLogLevel(), []],
+            /* When allowed error */
+            ["foo", "bar", new ErrorLogLevel(), []],
+            /* When allowed fatal */
+            ["foo", "bar", new FatalLogLevel(), []],
+        ];
+    }
+
+    /**
+     * @dataProvider testReceiveInfoDataProvider
+     * @param string $foo
+     * @param string $bar
+     * @param LogLevel $allowedLevel
+     * @param array $expected
+     * @return void
+     */
+    public function testReceiveInfo(string $foo, string $bar, LogLevel $allowedLevel, array $expected): void
+    {
+        $target = new FakeLogTarget();
+        $log = new Log(
+            $target,
+            new FakeLogLineFormat(self::SEPARATOR),
+            $allowedLevel
+        );
+        $log->receiveInfo(new FakeLogLine($foo, $bar));
+        $this->assertEquals($expected, $target->pullData());
+    }
+
+    public function testReceiveInfoDataProvider(): array
+    {
+        return [
+            /* When allowed debug */
+            ["foo", "bar", new DebugLogLevel(), ["info" . self::SEPARATOR . "foo" . self::SEPARATOR . "bar"]],
+            /* When allowed info */
+            ["foo", "bar", new InfoLogLevel(), ["info" . self::SEPARATOR . "foo" . self::SEPARATOR . "bar"]],
+            /* When allowed notice */
+            ["foo", "bar", new NoticeLogLevel(), []],
+            /* When allowed warning */
+            ["foo", "bar", new WarningLogLevel(), []],
+            /* When allowed error */
+            ["foo", "bar", new ErrorLogLevel(), []],
+            /* When allowed fatal */
+            ["foo", "bar", new FatalLogLevel(), []],
+        ];
+    }
+
+    /**
+     * @dataProvider testReceiveDebugDataProvider
+     * @param string $foo
+     * @param string $bar
+     * @param LogLevel $allowedLevel
+     * @param array $expected
+     * @return void
+     */
+    public function testReceiveDebug(string $foo, string $bar, LogLevel $allowedLevel, array $expected): void
+    {
+        $target = new FakeLogTarget();
+        $log = new Log(
+            $target,
+            new FakeLogLineFormat(self::SEPARATOR),
+            $allowedLevel
+        );
+        $log->receiveDebug(new FakeLogLine($foo, $bar));
+        $this->assertEquals($expected, $target->pullData());
+    }
+
+    public function testReceiveDebugDataProvider(): array
+    {
+        return [
+            /* When allowed debug */
+            ["foo", "bar", new DebugLogLevel(), ["debug" . self::SEPARATOR . "foo" . self::SEPARATOR . "bar"]],
+            /* When allowed info */
+            ["foo", "bar", new InfoLogLevel(), []],
+            /* When allowed notice */
+            ["foo", "bar", new NoticeLogLevel(), []],
+            /* When allowed warning */
+            ["foo", "bar", new WarningLogLevel(), []],
+            /* When allowed error */
+            ["foo", "bar", new ErrorLogLevel(), []],
             /* When allowed fatal */
             ["foo", "bar", new FatalLogLevel(), []],
         ];
