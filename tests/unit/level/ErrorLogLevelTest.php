8fa2383d8192a6772f2aa41d7c5833f16eb47e97
kikimarik <kikimarik@inbox.ru>
2022-06-19 20:32:38 +0300

    Unit tests - add testWeigh for ErrorLogLevel - add testAssertLessThenOrEqual for ErrorLogLevel

---
+++ tests/unit/level/ErrorLogLevelTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace kikimarik\lognote\tests\unit\level;
+
+use Codeception\Test\Unit;
+use kikimarik\lognote\core\LogLevel;
+use kikimarik\lognote\level\DebugLogLevel;
+use kikimarik\lognote\level\ErrorLogLevel;
+use kikimarik\lognote\level\FatalLogLevel;
+use kikimarik\lognote\level\InfoLogLevel;
+use kikimarik\lognote\level\NoticeLogLevel;
+use kikimarik\lognote\level\WarningLogLevel;
+
+final class ErrorLogLevelTest extends Unit
+{
+    /**
+     * @return void
+     */
+    public function testWeigh(): void
+    {
+        $level = new ErrorLogLevel();
+        $result = $level->weigh();
+        $this->assertEquals(5, $result);
+    }
+
+    /**
+     * @dataProvider testAssertLessThenOrEqualProvider
+     * @param LogLevel $inputLevel
+     * @param bool $expected
+     * @return void
+     */
+    public function testAssertLessThenOrEqual(LogLevel $inputLevel, bool $expected): void
+    {
+        $level = new ErrorLogLevel();
+        $result = $level->assertLessThenOrEqual($inputLevel);
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testAssertLessThenOrEqualProvider(): array
+    {
+        return [
+            /* Imaginary log level with 0 weight for example */
+            [new FakeLogLevel("fake", 0), false],
+            /* Debug log level */
+            [new DebugLogLevel(), false],
+            /* Info log level */
+            [new InfoLogLevel(), false],
+            /* Notice log level */
+            [new NoticeLogLevel(), false],
+            /* Warning log level */
+            [new WarningLogLevel(), false],
+            /* Error log level */
+            [new ErrorLogLevel(), true],
+            /* Fatal log level */
+            [new FatalLogLevel(), true],
+        ];
+    }
+}
